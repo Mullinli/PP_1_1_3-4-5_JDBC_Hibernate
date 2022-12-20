3b90ae4fb1a4471cdc9a687ac70df9d8209b3865
yaroslav <ylatushkov@yahoo.com>
2022-12-20 22:00:45 +0300

    1.1.4 done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.saveUser("Yaroslav", "Latushkov", (byte) 20);
+        System.out.println(userService.getAllUsers().get(0));
     }
 }
